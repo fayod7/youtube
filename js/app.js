eb1c4edf4da72872a3af3efa6688ec336df96331
fayod7 <fayodnv@gmail.com>
2025-05-29 13:46:40 +0500

    7

--- js/app.js
+++ js/app.js
@@ -18,15 +18,4 @@ const profileEl = document.querySelector(".profile__detail")
 function profileShow(){
     profileEl.classList.toggle("active")
 }
-const settingsEl = document.querySelector(".setting")
-function settingsShow(){
-    settingsEl.classList.toggle("active")
-}
-const shareEl = document.querySelector(".share-img")
-function shareShow(){
-    shareEl.classList.toggle("active")
-}
-const reportEl = document.querySelector(".reports")
-function reportShow(){
-    reportEl.classList.toggle("active")
-}
+
